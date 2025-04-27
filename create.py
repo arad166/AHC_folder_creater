1513d530ea40a74ca71d9a6e3282831411fbf8b9
arad166 <1a88.19e7@gmail.com>
2025-04-27 16:45:32 +0900

    update

--- create.py
+++ create.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 # 新しいフォルダの作成
-new_folder = r"../ahc045"
+new_folder = r"../ahc046"
 os.makedirs(new_folder, exist_ok=True)  # 既にフォルダがある場合はエラーを出さずに続行
 
 # コピーするファイルのパス（元ファイル）
--- run.sh
+++ run.sh
@@ -3,11 +3,12 @@
 # 引数が渡されていない場合はデフォルトで "0000" を使用
 input_file="${1:-0000}"
 
-cd "/home/arad166/cp/heur/ahcXXX/src" || exit 1
+cd "/home/arad166/cp/heur/ahcxxx/src" || exit 1
 g++ -O0 -std=c++17 -Wall -D_GLIBCXX_DEBUG -I ac-library main.cpp -o main
-./main < "tools/in/$input_file.txt" > "../out.txt"
+cd "/home/arad166/cp/heur/ahcxxx" || exit 1
+src/./main < "tools/in/$input_file.txt" > "out.txt"
 
-cd "/home/arad166/cp/heur/ahcXXX/" || exit 1
+cd "/home/arad166/cp/heur/ahcxxx/" || exit 1
 
 # ファイルの内容をクリップボードにコピー
 cat out.txt | xclip -selection clipboard
