68baba9036afd1792b31dbd9387f46ec7ace4e7e
arad166 <1a88.19e7@gmail.com>
2025-05-11 10:29:45 +0900

    コンパイルオプションを使い分けられるようにした

--- run.sh
+++ run.sh
@@ -4,11 +4,20 @@
 input_file="${1:-0000}"
 
 cd "/home/arad166/cp/heur/ahcxxx/src" || exit 1
-g++ -O0 -std=c++17 -Wall -D_GLIBCXX_DEBUG -I ac-library main.cpp -o main
+
+# DEBUG=1 を指定するとデバッグビルドになる
+if [ "$DEBUG" = "1" ]; then
+    echo "Building in debug mode...(slow)"
+    g++ -O2 -std=c++23 -Wall -D_GLIBCXX_DEBUG -I ac-library main.cpp -o main.out
+else
+    echo "Building in release mode...(fast)"
+    g++ -O3 -std=c++23 -Wall -I ac-library main.cpp -o main.out
+fi
+
 cd "/home/arad166/cp/heur/ahcxxx" || exit 1
+
+# 実行
 src/./main < "tools/in/$input_file.txt" > "out.txt"
 
-cd "/home/arad166/cp/heur/ahcxxx/" || exit 1
-
-# ファイルの内容をクリップボードにコピー
+# 結果をクリップボードにコピー
 cat out.txt | xclip -selection clipboard
